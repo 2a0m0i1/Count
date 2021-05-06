b59839a91244b37a9a15d9490b2227b6b2753aed
Ami Abe <abe.ami0702@gmail.com>
2021-05-06 23:11:06 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by 阿部亜未 on 2021/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
